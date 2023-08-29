9a1b4763a5241ccf96f7c6d51c8d21cf52f89e05
Arjav Awasthi <arjav.awasthi@hcl.com>
2023-08-29 12:50:44 +0530

    api-server

---
+++ api-server/src/main/java/com/learning/apiserver/ApiServerApplication.java
@@ -0,0 +1,15 @@
+package com.learning.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}
